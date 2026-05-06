6ae7ffe5925b65b5248313692a530cf05948a590
Sulaymaanabubakr <abubakrysulaimaan24434@gmail.com>
2026-05-06 09:33:52 +0100

    refactor: remove unused useLocation hook from Gallery page component

--- src/pages/public/Gallery.tsx
+++ src/pages/public/Gallery.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { useLocation } from 'react-router-dom';
 import { X, ZoomIn, Loader, Play, Film, ChevronLeft, Calendar, Tag } from 'lucide-react';
 import SEO from '../../components/common/SEO';
 import { useAnimations } from '../../hooks/useAnimations';
